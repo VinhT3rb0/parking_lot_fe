41a76a9e38512571132b8a02f0a53ada7c2758b0
nghiep2k3 <nguyennghiep1320@gmail.com>
2025-05-05 20:52:39 +0700

    code

--- src/pages/test/test2.tsx
+++ src/pages/test/test2.tsx
@@ -5,7 +5,7 @@ const Test2 = () => {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-blue-500">
-            <h1 className="text-4xl font-bold text-white mb-4">Tailwind CSS is working!</h1>
+            <h1 className="text-4xl font-bold text-white mb-4 uppercase">Tailwind CSS is working!</h1>
             <button className="px-6 py-2 bg-white text-blue-500 font-semibold rounded-lg shadow-md hover:bg-gray-100 font-bold uppercase">
                 Click Me
             </button>
